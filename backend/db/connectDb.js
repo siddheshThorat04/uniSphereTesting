392837e3d14d5aa4566b087039283ec4406389e3
siddheshThorat04 <siddheshthorat2004@gmail.com>
2025-01-02 19:08:06 +0530

    ndsjkfaek

--- backend/db/connectDb.js
+++ backend/db/connectDb.js
@@ -2,7 +2,7 @@ const  mongoose =require ("mongoose");
 
 const connectDb = async () => {
     try {
-        mongoose.connect("mongodb+srv://siddheshthorat2004:08xUCdQ5X1kG6A0E@cluster0.uj4qs.mongodb.net/uniSphere?retryWrites=true&w=majority&appName=Cluster0",)
+        mongoose.connect("mongodb+srv://siddheshthorat2004:08xUCdQ5X1kG6A0E@cluster0.uj4qs.mongodb.net/uniSphere2?retryWrites=true&w=majority&appName=Cluster0",)
             .then(() => console.log('Connected to MongoDB successfully'))
             .catch(err => console.error('Failed to connect to MongoDB:', err));
         console.log(`MongoDB Connected`);
