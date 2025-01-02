3f9f63952761bcb3e9764d0fd04363d17bc5fb5f
siddheshThorat04 <siddheshthorat2004@gmail.com>
2025-01-02 18:17:32 +0530

    vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
-export default {
-	build: {
-	  outDir: 'dist',  // Make sure this is pointing to 'dist'
-	  // Other build options
-	},
-  };
-  
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [react()],
+});
